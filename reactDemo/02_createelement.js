dbca11783d34758347e80d8906eda101e1ae82fd
npc <1700969761@qq.com>
2023-03-23 20:05:26 +0800

    疑惑，感觉不是很懂

--- reactDemo/02_createelement.js
+++ reactDemo/02_createelement.js
@@ -1,5 +1,19 @@
 console.log("eiffel is running ..");
 
+// 创建要使用的库 huriki
+
+const huriki = {
+    createElement
+}
+const element = huriki.createElement(
+    "div",
+    { id: "foo" },
+    huriki.createElement("a", null, "bar"),
+    huriki.createElement("b")
+)
+
+
+
 // 构建返回的对象
 function createElement(type, props, ...children) {
     return {
@@ -15,6 +29,10 @@ function createElement(type, props, ...children) {
 // 创建对应类型的 child 
 function createTextElement(text) {
     return {
-        type: "TEXT_ELEMENT"
+        type: "TEXT_ELEMENT",
+        propss: {
+            nodeValue: text,
+            children: [],
+        }
     }
 }
